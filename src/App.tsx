f1e8bbf669a4044cf4f1c99001f8826832fb4df5
Nishant <nishantjain0206@gmail.com>
2025-12-13 22:48:02 +0530

    login.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,8 @@ import CreateAdmin from './pages/CreateAdmin';
 import Dashboardadmin from "./pages/DashboardAdmin";
 import Dashboardpartner from "./pages/DashboardPartner";
 import Dashboarduser from "./pages/DashboardUser";
+import Login from "./pages/Login";
+
 
 function App() {
   return (
@@ -13,12 +15,12 @@ function App() {
         <Route path="/" element={<Home />}></Route>
         <Route path="/dashboardlayout" element={<DashboardLayout children={undefined} />}></Route>
         <Route path="/admin/create-admin" element={<CreateAdmin />}></Route>
-        <Route path="/admindashboard" element={<Dashboardadmin/>}></Route>
-        <Route path="/partnerdashboard" element={<Dashboardpartner/>}></Route>
-        <Route path="/userdashboard" element={<Dashboarduser/>}></Route>
-                                      
-          {/* Add your dashboard pages here */}
-        
+        <Route path="/admindashboard" element={<Dashboardadmin />}></Route>
+        <Route path="/partnerdashboard" element={<Dashboardpartner />}></Route>
+        <Route path="/userdashboard" element={<Dashboarduser />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+        {/* Add your dashboard pages here */}
+
       </Routes>
     </BrowserRouter>
   );
