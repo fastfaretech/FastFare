e4abf9dc48d301a19b9ccdf72c57f0f40d00bade
venkatesh kadiri <venkateshkadiri@spidy.local>
2026-01-22 21:40:44 +0530

    remove hardcoded aws credential usage

--- src/aws.ts
+++ src/aws.ts
@@ -1,9 +1,5 @@
 import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
 
 export const dynamoClient = new DynamoDBClient({
-  region: "ap-south-1",
-  credentials: {
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
-  },
+  region: "ap-south-1"
 });
